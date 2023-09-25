c801a39f1dd14a9af0f4a749dc5adc2d18569095
Cole Brokamp <cole.brokamp@gmail.com>
2023-09-25 14:42:32 -0400

    try to R a different way

--- activate_connect.sh
+++ activate_connect.sh
@@ -7,6 +7,6 @@ export RSC_LICENSE=`aws secretsmanager get-secret-value --secret-id saltdev-d1-r
 systemctl restart rstudio-connect
 
 # add SMTP email credentials to rcon gcfg file
-Rscript ./update_rcon_ini.R
+R -e "source('./update_rcon_ini.R')"
 
 systemctl restart rstudio-connect
