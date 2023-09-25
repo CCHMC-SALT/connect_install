4c349c89fc475c27ad43e4e39acf77f9227fed96
Cole Brokamp <cole.brokamp@gmail.com>
2023-09-25 11:31:54 -0400

    finish adding smtp auth in R

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-bundle: activate_connect.sh appspec.yml install_connect.sh start_service.sh stop_service.sh files/rstudio-connect.gcfg
-	zip -v rcon.zip activate_connect.sh appspec.yml install_connect.sh start_service.sh stop_service.sh files/rstudio-connect.gcfg
+bundle: activate_connect.sh appspec.yml install_connect.sh start_service.sh stop_service.sh files/rstudio-connect.gcfg update_rcon_ini.R convert_smtp_password.py
+	zip -v rcon.zip activate_connect.sh appspec.yml install_connect.sh start_service.sh stop_service.sh files/rstudio-connect.gcfg update_rcon_ini.R convert_smtp_password.py
 
 deploy: rcon.zip
 	aws s3 --profile salty cp rcon.zip s3://saltdev-d1-codedeploy/rcon/rcon.zip
--- update_rcon_ini.R
+++ update_rcon_ini.R
@@ -1,15 +1,19 @@
 if (!require(ini)) install.packages("ini")
 if (!require(glue)) install.packages("glue")
 if (!require(jsonlite)) install.packages("jsonlite")
+if (!require(aws.signature)) install.packages("aws.signature")
+
+the_region <- "us-east-1"
 
 secrets <-
   system2("aws",
   c(
     "secretsmanager",
-    "--profile salty",
+    ## "--profile salty",
     "get-secret-value",
-    "--secret-id saltdev-d1-ses-send-user-secret",
-    "--region us-east-1 --output json"
+    "--secret-id", "saltdev-d1-ses-send-user-secret",
+    "--region", the_region,
+    "--output", "json"
   ),
   stdout = TRUE
   ) |>
@@ -21,15 +25,9 @@ secrets <-
 
 d_ini <- read.ini("files/rstudio-connect.gcfg")
 
+d_ini$SMTP$Host <- glue("email-smtp.{the_region}.amazonaws.com")
 d_ini$SMTP$User <- secrets$key
-d_ini$SMTP$Host
-d_ini$SMTP$Password
+# convert secret key to SMTP password using python script from aws
+d_ini$SMTP$Password <- system2("./convert_smtp_password.py", c(secrets$secret, the_region), stdout = TRUE)
 
 write.ini(d_ini, "files/rstudio-connect.gcfg")
-file.show("files/rstudio-connect.gcfg")
-
-d_ini$SMTP$Host
-d_ini$SMTP$Port
-d_ini$SMTP$Password
-
-Sys.getenv("secrets_string")
