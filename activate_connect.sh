f27feb8d0815d8fcae4cb0768cae95eb0a69e588
Cole Brokamp <cole.brokamp@gmail.com>
2023-09-25 15:13:22 -0400

    deactivate key before activiating it again

--- activate_connect.sh
+++ activate_connect.sh
@@ -2,6 +2,7 @@
 
 # activate license
 export RSC_LICENSE=`aws secretsmanager get-secret-value --secret-id saltdev-d1-rcon-activate-connect-secret --region us-east-1 --output json --query "SecretString"`
+/opt/rstudio-connect/bin/license-manager deactivate
 /opt/rstudio-connect/bin/license-manager activate $RSC_LICENSE
 
 systemctl restart rstudio-connect
